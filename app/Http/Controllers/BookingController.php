28a30dd218821f8a6cb2e70b642c703f0f09afe2
Misbahudin <mizzc0d3@gmail.com>
2025-03-16 17:17:35 +0700

    refactor: remove unused Auth facade from BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,7 +7,6 @@ use App\Models\Schedule;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Midtrans\Config;
 use Midtrans\Snap;
